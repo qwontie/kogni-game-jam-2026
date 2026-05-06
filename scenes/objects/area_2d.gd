2d6d649480562b60157aeb496d8073a3d4a03b19
nightlyshelf <shchurartemit@gmail.com>
2026-05-06 17:31:50 +0200

    fix: bullet

--- scenes/objects/area_2d.gd
+++ scenes/objects/area_2d.gd
@@ -3,7 +3,6 @@ extends Area2D
 @export var speed: float = 800.0
 
 func _ready():
-	# Connect the signal in code so it always works
 	body_entered.connect(_on_body_entered)
 	$VisibleOnScreenNotifier2D.screen_exited.connect(_on_visible_on_screen_notifier_2d_screen_exited)
 
@@ -14,6 +13,8 @@ func _on_visible_on_screen_notifier_2d_screen_exited():
 	queue_free()
 
 func _on_body_entered(body):
+	if body.is_in_group("player"):
+		return  # ignore player
 	if body.has_method("take_damage"):
 		body.take_damage()
 	queue_free()
